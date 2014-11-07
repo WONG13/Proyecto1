1da7a4ca03ff5b59a7be554fc049d1b3a3ce9bf2
Rubén Vazquez <vazquezt101@gmail.com>
2014-11-06 23:56:20 -0800

    clase Usuario
    Se agregó la clase usuario

---
+++ InterfacesSief/InterfacesSief/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SiEFlib
+{
+    public class Usuario
+    {
+        private int CodUsu;
+        private string nomUsu;
+        private string contUsu;
+        public enum Permiso { interesado, lider_proyecto, empleado };
+        private Permiso permiso;
+
+        public Usuario()
+        {}
+
+        public void abrirSesion()
+        {
+            
+        }
+    }
+}
