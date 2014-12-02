eb4eb5e890d44d59d473f3f90a933d31233929dd
Rubén Vazquez <vazquezt101@gmail.com>
2014-12-02 01:42:15 -0800

    Cambios de Wong

--- InterfacesSief/InterfacesSief/Interfaces/E_MosRep.Designer.cs
+++ InterfacesSief/InterfacesSief/Interfaces/E_MosRep.Designer.cs
@@ -31,6 +31,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtInfoReporte = new System.Windows.Forms.TextBox();
             this.dgvReportes = new System.Windows.Forms.DataGridView();
+            this.txtTipoRep = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvReportes)).BeginInit();
             this.SuspendLayout();
@@ -42,6 +44,8 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtTipoRep);
             this.groupBox1.Controls.Add(this.dgvReportes);
             this.groupBox1.Controls.Add(this.txtInfoReporte);
             this.groupBox1.Controls.Add(this.label2);
@@ -50,10 +54,12 @@
             // btnAceptar
             // 
             this.btnAceptar.Location = new System.Drawing.Point(466, 338);
+            this.btnAceptar.Visible = false;
             // 
             // btnCancel
             // 
             this.btnCancel.Location = new System.Drawing.Point(547, 338);
+            this.btnCancel.Visible = false;
             // 
             // label2
             // 
@@ -67,19 +73,42 @@
             // 
             // txtInfoReporte
             // 
-            this.txtInfoReporte.Location = new System.Drawing.Point(302, 49);
+            this.txtInfoReporte.Location = new System.Drawing.Point(302, 76);
             this.txtInfoReporte.Multiline = true;
             this.txtInfoReporte.Name = "txtInfoReporte";
-            this.txtInfoReporte.Size = new System.Drawing.Size(280, 150);
+            this.txtInfoReporte.ReadOnly = true;
+            this.txtInfoReporte.Size = new System.Drawing.Size(280, 123);
             this.txtInfoReporte.TabIndex = 8;
             // 
             // dgvReportes
             // 
+            this.dgvReportes.AllowUserToAddRows = false;
+            this.dgvReportes.AllowUserToDeleteRows = false;
             this.dgvReportes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvReportes.Location = new System.Drawing.Point(10, 49);
             this.dgvReportes.Name = "dgvReportes";
+            this.dgvReportes.ReadOnly = true;
+            this.dgvReportes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvReportes.Size = new System.Drawing.Size(273, 150);
             this.dgvReportes.TabIndex = 10;
+            this.dgvReportes.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvReportes_CellMouseClick);
+            // 
+            // txtTipoRep
+            // 
+            this.txtTipoRep.Location = new System.Drawing.Point(392, 48);
+            this.txtTipoRep.Name = "txtTipoRep";
+            this.txtTipoRep.ReadOnly = true;
+            this.txtTipoRep.Size = new System.Drawing.Size(190, 20);
+            this.txtTipoRep.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(299, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Tipo de Reporte:";
             // 
             // E_MosRep
             // 
@@ -98,5 +127,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtInfoReporte;
         private System.Windows.Forms.DataGridView dgvReportes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTipoRep;
     }
 }
---
+++ InterfacesSief/InterfacesSief/Interfaces/I_Notificaciones.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace InterfacesSief.Interfaces
+{
+    partial class I_Notificaciones
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTipoRep = new System.Windows.Forms.TextBox();
+            this.txtInfoNotificacion = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-2, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(735, 41);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Notificaciones";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtTipoRep);
+            this.groupBox1.Controls.Add(this.txtInfoNotificacion);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 102);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(713, 317);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(314, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Asunto:";
+            // 
+            // txtTipoRep
+            // 
+            this.txtTipoRep.Location = new System.Drawing.Point(363, 48);
+            this.txtTipoRep.Name = "txtTipoRep";
+            this.txtTipoRep.ReadOnly = true;
+            this.txtTipoRep.Size = new System.Drawing.Size(271, 20);
+            this.txtTipoRep.TabIndex = 13;
+            // 
+            // txtInfoNotificacion
+            // 
+            this.txtInfoNotificacion.Location = new System.Drawing.Point(314, 74);
+            this.txtInfoNotificacion.Multiline = true;
+            this.txtInfoNotificacion.Name = "txtInfoNotificacion";
+            this.txtInfoNotificacion.ReadOnly = true;
+            this.txtInfoNotificacion.Size = new System.Drawing.Size(320, 104);
+            this.txtInfoNotificacion.TabIndex = 12;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(275, 125);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // I_Notificaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "I_Notificaciones";
+            this.Size = new System.Drawing.Size(741, 400);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        protected System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtInfoNotificacion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTipoRep;
+    }
+}
